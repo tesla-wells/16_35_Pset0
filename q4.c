c9fa0090e253a29c0ceff77840d112cd5b153529
Tesla Wells <teslawells@Andrew.localdomain>
2019-03-07 03:18:46 -0500

    initial changes

--- q4.c
+++ q4.c
@@ -11,11 +11,39 @@ typedef struct Node {
 } node; 
 
 void insert(node* root, char* str) {
-
+	if(strcmp(root->item, str) > 0){
+		if(root->upper == NULL){
+			node* newNode = malloc(sizeof(node));
+			newNode->upper = NULL;
+			newNode->lower = NULL;
+			newNode->item = str;
+			root->upper = newNode;
+		} else {
+			insert(root->upper, str);
+		}
+	} else {
+		if(root->lower == NULL){
+			node* newNode = malloc(sizeof(node));
+			newNode->upper = NULL;
+			newNode->lower = NULL;
+			newNode->item = str;
+			root->lower = newNode;
+		} else {
+			insert(root->lower, str);
+		}
+	}
 }
 
 void print_tree(node* root){
-
+	if(root->lower != NULL){
+		print_tree(root->lower);
+	} 
+	
+	printf("%s ", root->item);
+	
+	if(root->upper != NULL){
+		print_tree(root->upper);
+	}
 }
 
 void free_tree(node* root){
@@ -31,10 +59,10 @@ int main(int argc, char** argv){
 	root->upper = NULL;
 	root->lower = NULL;
 	root->item = "";
-	for(int i = 2; i < argc; i++){
-//		insert(root, argv[i])
+	for(int i = 1; i < argc; i++){
+		insert(root, argv[i]);
 	}
-//	print_tree(root);
-//	free_tree(root);
+	print_tree(root);
+	free_tree(root);
 }
 
